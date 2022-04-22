3ca07f25750f80d8c31b36a6bb123ba62344bca7
汪立超 <wanglichao.1121@163.com>
2022-04-22 10:32:31 +0800

    💄 import antd tooltip

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,11 +1,21 @@
 import {
     UserOutlined
 } from '@ant-design/icons'
+import { Tooltip } from 'antd'
 function UserButton(){
     return (
-        <button type="button" className='user-button'>
-            <UserOutlined/>
-        </button>
+        <Tooltip
+            placement="bottomLeft"
+            title={'wow'}
+            arrowPointAtCenter
+            //trigger='hover'
+            visible={true}
+            overlayClassName='user-tooltip'
+        >
+            <button type="button" className='user-button'>
+                <UserOutlined/>
+            </button>
+        </Tooltip>
     )
 }
 export default UserButton
