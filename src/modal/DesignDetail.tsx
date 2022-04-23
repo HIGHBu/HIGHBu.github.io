628c2ee524e5f031776e08dbce7525705c7f728e
汪立超 <wanglichao.1121@163.com>
2022-04-23 23:03:14 +0800

    📦 design-detail right-rol

--- src/modal/DesignDetail.tsx
+++ src/modal/DesignDetail.tsx
@@ -1,7 +1,7 @@
-import { DoubleRightOutlined, LeftOutlined, SmileOutlined } from "@ant-design/icons"
+import { DoubleRightOutlined, LeftOutlined, ShareAltOutlined, SmileOutlined, StarOutlined } from "@ant-design/icons"
 import { commentPlaceholder, showMoreComment } from "../text"
 import demoPic from '../assets/APP.png'
-import { Input } from "antd"
+import { Input, Pagination } from "antd"
 const { TextArea } = Input
 function DesignDetail(){
     return (
@@ -20,6 +20,21 @@ function DesignDetail(){
                 <button type='button'>
                     <SmileOutlined/>
                 </button>
+            </div>
+        </div>
+        <div className="right-col">
+            <img src={demoPic}/>
+            <div className="nav-and-action">
+                <div/>
+                <Pagination total={5}/>
+                <div className="actions">
+                    <button type='button'>
+                        <StarOutlined/>
+                    </button>
+                    <button type='button'>
+                        <ShareAltOutlined/>
+                    </button>
+                </div>
             </div>
         </div>
         <button type='button' className='back-button'>
