9f307904d0ca3289fbb5582eb9d5af0278338d2f
汪立超 <wanglichao.1121@163.com>
2022-04-23 14:08:22 +0800

    📦 tooltip content

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -5,13 +5,17 @@ import { Tooltip } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from '../store'
 import { Login } from '../store/userSlice'
+import { changeNameAvatar, generateTicket } from '../text'
 function UserToolTip(){
     const username=useSelector<RootState,string>((state)=>state.userSlice.username)
     return (
-        <div>
-            <span>{username}</span>
+        <div className="user-tooltip-content">
+            <p className="subtitle">
+                {username}
+            </p>
             <div>
-                something
+                <a className="text">{changeNameAvatar}</a>
+                <a className="text">{generateTicket}</a>
             </div>
         </div>
     )
---
+++ src/text.ts
@@ -0,0 +1,2 @@
+export const changeNameAvatar='修改昵称/头像'
+export const generateTicket='生成我的观战纪念票'
