6b6196e38a1552c2a4c96a781f5eff46ca8449b4
汪立超 <wanglichao.1121@163.com>
2022-05-07 00:05:16 +0800

    category processing

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -4,8 +4,8 @@ import {
 } from '@ant-design/icons'
 import { Menu, MenuProps } from 'antd';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../store/store';
 import { showDisignDetail } from '../store/modalSlice';
 function MenuButton(){
     const [collapsed, setCollapsed] = React.useState(false);
@@ -13,54 +13,8 @@ function MenuButton(){
     const handleSelect=()=>{
         dispatch(showDisignDetail())
     }
-    const menuItems:Required<MenuProps>['items'] = [{
-        label: '实体产品展厅',
-        icon: <MailOutlined/>,
-        key: 'shiti',
-        children: [{
-            label: '智慧生活',
-            key: 'shenghuo',
-            children: [{
-                label: '产品1',
-                key: 'p1'
-            },{
-                label: '产品2',
-                key: 'p2'
-            }]
-        },{
-            label: '家居设计',
-            key: 'jiaju',
-            children: [{
-                label: '产品1',
-                key: 'p1'
-            },{
-                label: '产品2',
-                key: 'p2'
-            }]
-        },{
-            label: '趣味摆件',
-            key: 'baijian',
-            children: [{
-                label: '产品3',
-                key: 'p3'
-            },{
-                label: '产品4',
-                key: 'p4'
-            }]
-        }]
-    },{
-        label: '体验设计',
-        icon: <MailOutlined/>,
-        key: 'tiyan',
-    },{
-        label: '移动应用',
-        icon: <MailOutlined/>,
-        key: 'yidong',
-    },{
-        label: '交互装置',
-        icon: <MailOutlined/>,
-        key: 'jiaohu',
-    }];
+    type ItemsType=Required<MenuProps>['items']
+    const menuItems:ItemsType = useSelector<RootState,ItemsType>((state=>state.exhibitSlice.category));
     return (
         <div className='menu-group'>
             <button type="button" className="menu-button" onClick={()=>setCollapsed(!collapsed)}>
--- src/store/exhibitSlice.ts
+++ src/store/exhibitSlice.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { MenuProps } from 'antd'
 import { Exhibit, fetchExhibits } from '../api/exhibit'
 
 export const updateExhibits = createAsyncThunk('exhibit/fetch',async()=>{
@@ -8,13 +9,47 @@ export const updateExhibits = createAsyncThunk('exhibit/fetch',async()=>{
 const exhibitSlice = createSlice({
   name: 'exhibit',
   initialState: {
-    items: [] as Exhibit[]
+    items: [] as Exhibit[],
+    category: [] as ({
+      key: string,
+      label: string,
+      children: ({
+        key: string,
+        label: string,
+        children: ({
+          key: string,
+          label:string
+        })[]
+      })[]
+    })[]
   },
   reducers: {},
   extraReducers: (builder)=>{
     builder.addCase(updateExhibits.fulfilled,(state,action)=>{
       state.items=action.payload
-      console.log(state.items)
+      action.payload.forEach(item=>{
+        const [fir,sec]=item.location.split('-')
+        let firdir=state.category.find(v=>v.key===fir)
+        if(firdir===undefined){
+          firdir=state.category[state.category.push({
+            key: fir,
+            label: fir,
+            children: []
+          })-1]
+        }
+        let secdir=firdir.children.find(v=>v.key===sec)
+        if(secdir===undefined){
+          secdir=firdir.children[firdir.children.push({
+            key: sec,
+            label: sec,
+            children: []
+          })-1]
+        }
+        secdir.children.push({
+          key: item.id,
+          label: item.title
+        })
+      })
     })
   }
 })
