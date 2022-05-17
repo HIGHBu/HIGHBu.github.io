6c156287ccf8bc286a6e09381d024e548a973341
汪立超 <wanglichao.1121@163.com>
2022-05-18 00:31:26 +0800

    login type prompt

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,15 @@
+import { QuestionCircleOutlined } from '@ant-design/icons'
+import { Checkbox, Tooltip } from 'antd'
 import { MouseEventHandler, useState } from 'react'
 import bg from '../assets/card-bg.png'
-import { guestLogin,userLogin } from '../text'
+import { agreement, guestLogin,loginTypePrompt1,loginTypePrompt2,startVisit,userLogin } from '../text'
 import { WelcomeProps } from './Welcome'
+function Prompt(){
+    return (<div>
+        <p>{loginTypePrompt1}</p>
+        <p>{loginTypePrompt2}</p>
+    </div>)
+}
 function Login(props:WelcomeProps){
     const {onExit}=props
     const [isUser,setisUser]=useState(false)
@@ -9,11 +17,19 @@ function Login(props:WelcomeProps){
     const selectUser=()=>setisUser(true)
     return (<div id='login-page'>
         <div id='login-select'>
-            <span className={isUser?undefined:'selected'} onClick={selectGuest}>{guestLogin}</span>
+            <span className={isUser?'unselected':'selected'} onClick={selectGuest}>{guestLogin}</span>
             <span>|</span>
-            <span className={isUser?'selected':undefined} onClick={selectUser}>{userLogin}</span>
+            <span className={isUser?'selected':'unselected'} onClick={selectUser}>{userLogin}</span>
+            <Tooltip title={Prompt}>
+                <QuestionCircleOutlined/>
+            </Tooltip>
         </div>
         <img src={bg}/>
+        <div id='check'>
+            <Checkbox/>
+            <span>{agreement}</span>
+        </div>
+        <h1>{startVisit}</h1>
     </div>)
 }
 export default Login
