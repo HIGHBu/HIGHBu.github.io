a5d827a025411f63bfbae994330da8d7d8feaaa6
汪立超 <wanglichao.1121@163.com>
2022-07-05 21:58:51 +0800

    compatible for dev

--- src/main.tsx
+++ src/main.tsx
@@ -14,8 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/gallery_ui/" element={<App/>}/>
-          <Route path="/gallery_ui/show/:path" element={<Exhibitshow/>}/>
+          <Route path={import.meta.env.BASE_URL} element={<App/>}/>
+          <Route path={import.meta.env.BASE_URL+"/show/:path"} element={<Exhibitshow/>}/>
         </Routes>
       </BrowserRouter>
     </Provider>
